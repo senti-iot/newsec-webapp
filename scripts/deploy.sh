5792a8eefa586eadb4ba47926a42798176452065
Henrik Hansen <hh@fsck.dk>
2020-07-01 12:39:45 +0200

    travis stuff

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,6 +16,6 @@ if [[ "$1" == "dev" ]]; then
 	rsync -r --quiet $2/build/ deploy@rey.webhouse.net:/srv/www/newsec-webapp/dev
 	echo
 	curl -X POST -H 'Content-type: application/json' --data '{"text":"newsec-webapp DEV updated!"}' https://hooks.slack.com/services/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
-	echo Deployment to beta done!
+	echo Deployment to dev done!
 	exit 0
 fi
