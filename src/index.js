2b94d0959a36cc1a549250a4bad01dda79c2b6b1
Andrei Tudor <andrei.tudor27@gmail.com>
2020-08-03 17:26:09 +0200

    Fix the Service Worker

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import store from './app/store';
+import store from './redux/store';
 // import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 // import { ThemeProvider, CssBaseline } from '@material-ui/core';
@@ -13,10 +13,10 @@ import { updateServiceworker } from 'redux/serviceWorkerRedux';
 import Providers from './Providers'
 
 const onUpdate = () => {
-	store.dispatch(updateServiceworker())
+	store().dispatch(updateServiceworker())
 }
 
-serviceWorker.register({ onUpdate: onUpdate });
+serviceWorker.register({ onUpdate: onUpdate, onSuccess: () => console.log('Here can be dispatched a message for succesfully updated') });
 
 ReactDOM.render(
 	<React.StrictMode>
