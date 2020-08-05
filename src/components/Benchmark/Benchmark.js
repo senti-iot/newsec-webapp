d2f3acfa156132fc3d8cea7886a1c180473ec603
Henrik Hansen <hh@fsck.dk>
2020-08-05 12:07:15 +0200

    fix title

--- src/components/Benchmark/Benchmark.js
+++ src/components/Benchmark/Benchmark.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
 
 import OverviewBarGraph from 'components/Overview/OverviewBarGraph';
 import OverviewScore from 'components/Overview/OverviewScore';
 import OverviewForecast from 'components/Overview/OverviewForecast';
 import BenchmarkEnergyGraph from 'components/Benchmark/BenchmarkEnergyGraph';
+import { changeHeaderTitle, changeSecondaryBarShown } from 'redux/appState';
 
 const Benchmark = props => {
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(changeSecondaryBarShown(false));
+		dispatch(changeHeaderTitle('Benchmark'));
+	}, [dispatch]);
+
 	return (
 		<>
 			<Grid container spacing={5}>
