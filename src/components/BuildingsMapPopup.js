64de388516085e514bb0fdb2c98438d12016c984
Henrik Hansen <hh@fsck.dk>
2020-08-05 19:10:15 +0200

    map popup

---
+++ src/components/BuildingsMapPopup.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Typography, Button } from '@material-ui/core';
+import { useHistory } from 'react-router';
+
+const BuildingsMapPopup = props => {
+	const building = props.building;
+	const history = useHistory();
+
+	const handleClick = () => {
+		history.push('/building/' + building.uuid);
+	}
+
+	return (
+		<>
+			<Typography variant="h4">{building.name}</Typography>
+			<Typography variant="h5" style={{ color: '#979797' }}>{building.no}</Typography>
+
+			<Button onClick={handleClick} style={{ width: '100%', marginTop: 30, color: '#fff', backgroundColor: '#365979' }}>Se detaljer</Button>
+		</>
+	)
+}
+
+export default BuildingsMapPopup;
