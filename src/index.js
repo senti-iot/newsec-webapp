7f611841580854ec0d1e0022fffcae2b346e2c74
Henrik Hansen <hh@fsck.dk>
2020-08-06 12:04:56 +0200

    tja

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import store from './redux/store';
+import store from './redux/store';
 // import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 // import { ThemeProvider, CssBaseline } from '@material-ui/core';
-// import { updateServiceworker } from 'redux/serviceWorkerRedux';
+import { updateServiceworker } from 'redux/serviceWorkerRedux';
 
 import Providers from './Providers'
 
 const onUpdate = () => {
-	window.reload();
+	store().dispatch(updateServiceworker())
 }
 
 serviceWorker.register({ onUpdate: onUpdate, onSuccess: () => console.log('Here can be dispatched a message for succesfully updated') });
